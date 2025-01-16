d008a0e70920ade9160ec53a3c9df160c6e6b9e3
pedrohmtoledo <toledopedro.hmt@gmail.com>
2025-01-16 18:38:57 +0000

    Create the index boilerplate

---
+++ src/index.js
@@ -0,0 +1,10 @@
+console.log("dasda")
+import "./style.css"
+
+import foodImg from "./food.png"
+
+const content = document.querySelector("#content");
+const image = document.createElement("img");
+
+image.src = foodImg;
+
