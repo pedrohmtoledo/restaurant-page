94410705a4cb99cc07189bf4e99b3aa19f0cbae5
pedrohmtoledo <toledopedro.hmt@gmail.com>
2025-01-19 13:55:51 +0000

    Edit about page css and html

--- src/about.js
+++ src/about.js
@@ -8,8 +8,31 @@ function renderAboutPage() {
     const aboutPage  = `
     <h2> Food To Eat Information </h2>
     <div class="information">
-    address
-    contact
+        <div class="hours">
+            <h3> Hours </h3>
+                <ul class="hourslist about">
+                <li>Monday: We dont work on mondays</li>
+                <li>Tuesday: 1pm - 10pm </li>
+                <li>Wednesday: 10am - 7pm</li>
+                <li>Thursday: Break due High workload</li>
+                <li>Friday: We might work, just come</li>
+                <li>Saturday: Only at night, premier league day</li>
+                <li>Sunday: fuck off</li>
+            </ul>
+        </div>
+        <div class="adress">
+            <h3> Address </h3>
+            <p>1234 Yum Yum Street, Flavor Town, Planet Snackos, ZIP Code: 56789.</p>
+        </div>
+        <div class="contact">
+            <h3> Contact info </h3>
+            <ul class="contactlist about">
+                <li> Phone: 1-800-EAT-FOOD </li>
+                <li> Email: nomnom@foodtoeat.biz</li>
+                <li> Website: www.foodtoeatwow.com</li>
+                <li> Fax (because why not): 555-FOOD-FAX</li>
+            </ul>
+
     </div>`;
 
         
@@ -18,7 +41,7 @@ function renderAboutPage() {
     aboutButton.classList.add("button-active");
 
     aboutPageContent.innerHTML = aboutPage;
-    aboutPageContent.classList.add("homepage");
+    aboutPageContent.classList.add("aboutpage");
     content.appendChild(aboutPageContent);
 }
 
--- src/menu.js
+++ src/menu.js
@@ -8,12 +8,42 @@ function renderMenuPage() {
     const menuPage  = `
     <h2> Food To Eat Menu </h2>
     <div class="menu">
-        <ul class="menulist">
-            <li>Starter</li>
-            <li>Main</li>
-            <li>Dessert</li>
-            <li>Drinks</li>
-        </ul>
+
+        <div class="starters menulist">
+            <h3> Starters(eat this first)</h3>
+            <ul class="starterlist">
+                <li><span class="item">Maybe Soup</span><span class="dots"> &nbsp;</span><span class="price">Some money</span></li>
+                <li><span class="item">Meat Salad </span><span class="dots"> &nbsp;</span><span class="price">Some money</span></li>
+                <li><span class="item">That Bread Thing </span><span class="dots"> &nbsp;</span><span class="price">Few money</span></li>
+            </ul>
+        </div>
+
+        <div class="mains menulist">
+            <h3> Mains </h3>
+            <ul class="mainslist">
+                <li><span class="item">Italian whatever</span><span class="dots"> &nbsp;</span><span class="price">Cheap(always)</span></li>
+                <li><span class="item">Salad Meat</span><span class="dots"> &nbsp;</span><span class="price">Loan </span></li>
+                <li><span class="item">Spicy Asian Food</span><span class="dots"> &nbsp;</span><span class="price">Few money</span></li>
+            </ul>
+        </div>
+
+        <div class="desserts menulist">
+            <h3> Desserts </h3>
+            <ul class="starterlist">
+                <li><span class="item">Cake</span><span class="dots"> &nbsp;</span><span class="price">$5.00(need money)</span></li>
+                <li><span class="item">Something with pistaccio</span><span class="dots"> &nbsp;</span><span class="price">$1.00</span></li>
+                <li><span class="item">Ice Cream</span><span class="dots"> &nbsp;</span><span class="price">Money</span></li>
+            </ul>
+        </div>
+
+        <div class="drinks menulist">
+            <h3> Drinks </h3>
+            <ul class="starterlist">
+                <li><span class="item">Coffee</span><span class="dots"> &nbsp;</span><span class="price">Some Money</span></li>
+                <li><span class="item">Bo'o o' Wo'ah</span><span class="dots"> &nbsp;</span><span class="price">$0.10</span></li>
+                <li><span class="item">Beer</span><span class="dots"> &nbsp;</span><span class="price">$37.00</span></li>
+            </ul>
+        </div>
     </div>`;
         
     homeButton.classList.remove("button-active");
@@ -21,7 +51,7 @@ function renderMenuPage() {
     aboutButton.classList.remove("button-active");
 
     menuPageContent.innerHTML = menuPage;
-    menuPageContent.classList.add("homepage");
+    menuPageContent.classList.add("menupage");
     content.appendChild(menuPageContent);
 }
 
